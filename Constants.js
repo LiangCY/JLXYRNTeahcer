e00b22b3993856a37195bebe172585044c1cd29a
LiangCY <liangcynju@qq.com>
2016-01-12 15:43:58 +0800

    修改URL

--- Constants.js
+++ Constants.js
@@ -1,8 +1,8 @@
-const URL_BASE = 'http://114.212.113.228:3030';
+const URL_BASE = 'http://219.219.117.114:3030';
 
 module.exports = {
-    URL_PREFIX: 'http://114.212.113.228',
-    URL_PREFIX_AVATAR: 'http://114.212.113.228/avatar/',
+    URL_PREFIX: 'http://219.219.117.114',
+    URL_PREFIX_AVATAR: 'http://219.219.117.114/avatar/',
     URL_LOGIN: URL_BASE + '/login',
     URL_LOGOUT: URL_BASE + '/logout',
     URL_USER_BASIC: URL_BASE + '/user_basic',
