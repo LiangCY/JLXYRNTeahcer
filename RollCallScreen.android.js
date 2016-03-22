14a87d1a853af465ff98d4c75811bfe6c6af88ec
LiangCY <liangcynju@qq.com>
2016-03-22 16:34:51 +0800

    修复点名页面列表更新时的BUG

--- RollCallScreen.android.js
+++ RollCallScreen.android.js
@@ -274,7 +274,6 @@ var styles = StyleSheet.create({
     row: {
         flex: 1,
         flexDirection: 'row',
-        overflow: 'hidden',
         alignItems: 'center',
         backgroundColor: 'white',
         padding: 12,
